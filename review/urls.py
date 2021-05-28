f3ea3abf2956a7b4d6a38817361419dba9342c79
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-29 00:13:35 +0300

    add search  funtionalities

---
+++ review/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    path('',views.welcome,name='Welcome'),
+    path('projects/',views.view_project,name='projects'),
+    path('search/', views.search_results, name='search_results'),
+    path('new-projects/', views.new_projects, name='new-project'),
+    path('ajax/newsletter/', views.newsletter, name='newsletter'),
+    path('api/projects/', views.MerchList.as_view()),
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- review/views.py
+++ review/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import Project,Profile
+from .forms import NewsLetterForm,NewArticleForm
+import datetime as dt
+from .email import send_welcome_email
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
-# Create your views here.
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_projects = Project.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
